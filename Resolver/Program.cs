d5599a11e37fbb777809b8d6bfbe0c914404a115
Nokita Kaze <admin@kanaria.ru>
2020-10-26 21:31:07 +0300

    * Add many domains * Resolver update

--- Resolver/Program.cs
+++ Resolver/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using DnsClient;
 using DnsClient.Protocol;
@@ -19,49 +20,99 @@ namespace Resolver
                 .Where(t => (t.Trim() != "") && !t.StartsWith("#"))
                 .Select(t => t.Trim())
                 .ToHashSet();
-            var lines = new List<string>();
-            foreach (var domain in domains)
-            {
-                var ips = await GetDomain(domain);
-                if (!ips.Any())
+            var usedDomains = new HashSet<string>();
+            var tasks = domains
+                .Select((domain, index) =>
                 {
-                    Console.WriteLine("Domain {0} contains no MX-elements", domain);
-                    continue;
-                }
+                    if (usedDomains.Contains(domain))
+                    {
+                        return (null, -1, null);
+                    }
 
-                var smtpLine = string.Concat(ips.Select(ip => string.Format(":[{0}]", ip)));
-                lines.Add(string.Format(
-                    "    echo '{0} smtp{1}' >> '/etc/mail/mailertable' && \\",
-                    domain,
-                    smtpLine
-                ));
-            }
+                    usedDomains.Add(domain);
 
-            File.WriteAllLines(args[1], lines);
+                    Task<ICollection<string>> task = GetMXIpForDomain(domain);
+                    return (domain, index, task);
+                })
+                .Where(t => t.domain != null)
+                .ToArray();
+
+            //
+            await Task.WhenAll(tasks.Select(t => t.task).ToArray());
+
+            var lines = tasks
+                .Select(t =>
+                {
+                    t.task!.Wait();
+                    var ips = t.task.Result;
+                    if (!ips.Any())
+                    {
+                        Console.WriteLine("Domain {0} contains no valid IPs for MX-records", t.domain);
+                        return null;
+                    }
+
+                    var smtpLine = string.Concat(ips.Select(ip => string.Format(":[{0}]", ip)));
+                    return string.Format(
+                        "    echo '{0} smtp{1}' >> '/etc/mail/mailertable'",
+                        t.domain,
+                        smtpLine
+                    );
+                })
+                .Where(t => t != null)
+                .Aggregate("", (a, b) => a + " && \\\n" + b);
+
+            File.WriteAllText(args[1], lines);
         }
 
-        private static async Task<ICollection<string>> GetDomain(string domain)
-        {
-            var lookup = new LookupClient();
-            var result = await lookup.QueryAsync(domain, QueryType.MX);
+        private static readonly SemaphoreSlim mutex = new SemaphoreSlim(5, 5);
 
-            Console.WriteLine("\nGet MX-records for {0}", domain);
+        private static async Task<ICollection<string>> GetMXIpForDomain(string domain)
+        {
+            await mutex.WaitAsync();
+            try
+            {
+                return await GetMXIpForDomainInner(domain);
+            }
+            finally
+            {
+                mutex.Release();
+            }
+        }
+
+        private static async Task<ICollection<string>> GetMXIpForDomainInner(string domain)
+        {
+            var lookup = new LookupClient(new LookupClientOptions()
+            {
+                Timeout = TimeSpan.FromMinutes(1),
+            });
+            var result = await DNSRequest(lookup, domain, QueryType.MX);
+
+            Console.WriteLine("{0,-30}:\tGet MX-records", domain);
             var mxDomains = result
                 .Answers
-                .Select(t => t as MxRecord)
-                .OrderBy(t => t!.Preference)
+                .OfType<MxRecord>()
+                .OrderBy(t => t.Preference)
                 .Select(t => t.Exchange.Value)
                 .ToArray();
 
-            Console.WriteLine("Got {0} MX-records", mxDomains.Length);
+            Console.WriteLine("{1,-30}:\tGot {0} MX-records", mxDomains.Length, domain);
             var allIps = new List<string>();
             foreach (var mxDomain in mxDomains)
             {
-                result = await lookup.QueryAsync(mxDomain, QueryType.ANY);
+                result = await DNSRequest(lookup, mxDomain, QueryType.ANY);
+                if (result.HasError)
+                {
+                    Console.WriteLine("{3,-30}:\tCan not get DNS records for {0} from {1}. Error: {2}",
+                        mxDomain,
+                        lookup.NameServers.First().Address,
+                        result.ErrorMessage,
+                        domain
+                    );
+                    continue;
+                }
+
                 var mxIps = result
                     .Answers
-                    .Where(t => (t.RecordType == ResourceRecordType.AAAA) ||
-                                (t.RecordType == ResourceRecordType.A))
                     .Select(t =>
                     {
                         return t switch
@@ -71,6 +122,7 @@ namespace Resolver
                             _ => null
                         };
                     })
+                    .Where(t => t != null)
                     .ToHashSet();
 
                 if (mxIps.Any())
@@ -79,19 +131,57 @@ namespace Resolver
                     continue;
                 }
 
-                var soaRaw = result
+                // https://tools.ietf.org/html/rfc8482
+                var rfcRestriction = result
                     .Answers
-                    .FirstOrDefault(t => t.RecordType == ResourceRecordType.SOA);
+                    .OfType<HInfoRecord>()
+                    .Any(x => x.Cpu.ToUpperInvariant() == "RFC8482");
+                DnsResourceRecord soaRaw;
+                if (rfcRestriction)
+                {
+                    result = await DNSRequest(lookup, mxDomain, QueryType.A);
+                    var a1 = result
+                        .Answers
+                        .OfType<ARecord>()
+                        .Select(t => t.Address.ToString())
+                        .ToList();
+
+                    result = await DNSRequest(lookup, mxDomain, QueryType.AAAA);
+                    var a2 = result
+                        .Answers
+                        .OfType<AaaaRecord>()
+                        .Select(t => t.Address.ToString())
+                        .ToArray();
+                    a1.AddRange(a2);
+
+                    if (a1.Any())
+                    {
+                        allIps.AddRange(a1);
+                        continue;
+                    }
+
+                    result = await DNSRequest(lookup, mxDomain, QueryType.SOA);
+
+                    soaRaw = result
+                        .Answers
+                        .FirstOrDefault(t => t.RecordType == ResourceRecordType.SOA);
+                }
+                else
+                {
+                    soaRaw = result
+                        .Answers
+                        .FirstOrDefault(t => t.RecordType == ResourceRecordType.SOA);
+                }
+
                 if (!(soaRaw is SoaRecord soa))
                 {
                     continue;
                 }
 
-                var soaResult = await lookup.QueryAsync(soa.MName, QueryType.ANY);
+                var soaResult = await DNSRequest(lookup, soa.MName, QueryType.ANY);
+
                 var soaARecord = soaResult
                     .Answers
-                    .Where(t => (t.RecordType == ResourceRecordType.AAAA) ||
-                                (t.RecordType == ResourceRecordType.A))
                     .Select(t =>
                     {
                         return t switch
@@ -101,18 +191,20 @@ namespace Resolver
                             _ => null
                         };
                     })
-                    .FirstOrDefault();
+                    .FirstOrDefault(t => t != null);
                 if (soaARecord == null)
                 {
                     continue;
                 }
 
-                var lookupLocal = new LookupClient(IPAddress.Parse(soaARecord));
-                result = await lookupLocal.QueryAsync(mxDomain, QueryType.ANY);
+                var lookupLocal = new LookupClient(new LookupClientOptions(IPAddress.Parse(soaARecord))
+                {
+                    Timeout = TimeSpan.FromMinutes(1),
+                });
+                result = await DNSRequest(lookupLocal, mxDomain, QueryType.ANY);
+
                 mxIps = result
                     .Answers
-                    .Where(t => (t.RecordType == ResourceRecordType.AAAA) ||
-                                (t.RecordType == ResourceRecordType.A))
                     .Select(t =>
                     {
                         return t switch
@@ -122,13 +214,54 @@ namespace Resolver
                             _ => null
                         };
                     })
+                    .Where(t => t != null)
                     .ToHashSet();
 
                 allIps.AddRange(mxIps);
             }
 
-            Console.WriteLine("Got {0} non unique A-/AAAA-records", allIps.Count);
+            Console.WriteLine("{1,-30}:\tGot {0} non unique A-/AAAA-records", allIps.Count, domain);
             return allIps.ToHashSet();
+        }
+
+        private static async Task<IDnsQueryResponse> DNSRequest(
+            ILookupClient client,
+            string domain,
+            DnsClient.QueryType type
+        )
+        {
+            IDnsQueryResponse lastError = null;
+
+            for (var i = 0; i < 10; i++)
+            {
+                try
+                {
+                    var result = await client.QueryAsync(domain, type);
+                    if (result.HasError)
+                    {
+                        Console.WriteLine("Can not get DNS records for {0}. Type {1} from {2}. Error: {3}",
+                            domain,
+                            type,
+                            client.NameServers.First().Address,
+                            result.ErrorMessage
+                        );
+                        lastError = result;
+                        continue;
+                    }
+
+                    return result;
+                }
+                catch (DnsResponseException)
+                {
+                    Console.WriteLine("Can not get DNS records for {0}. Type {1} from {2}",
+                        domain,
+                        type,
+                        client.NameServers.First().Address
+                    );
+                }
+            }
+
+            return lastError;
         }
     }
 }
